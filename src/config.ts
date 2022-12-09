93c1216fbfa5d09e72e7e620cdf01a9bf62f89c3
RGgt <rggt.inbox@gmail.com>
2022-12-09 17:18:08 +0200

    changed drawing target size

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,8 @@ export default {
   type: Phaser.AUTO,
   parent: 'game',
   scale: {
-    width: 1280,
-    height: 720,
+    width: 800,
+    height: 600,
     mode: Phaser.Scale.NONE,
     autoCenter: Phaser.Scale.CENTER_BOTH
   }
