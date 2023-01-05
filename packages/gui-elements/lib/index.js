8f0c0ec5aabf1bf9ca80de48b508cb1498a8259a
RGgt <rggt.inbox@gmail.com>
2023-01-05 23:05:17 +0200

    gui-element package is now Typescript only

--- packages/gui-elements/lib/index.js
+++
@@ -1,9 +0,0 @@
-export { default as Button } from './components/Button';
-export { default as PanelBox } from './components/PanelBox';
-export { default as GroupBox } from './components/GroupBox';
-export * from './creators/TextCreator';
-export { BackgroundCreator } from './creators/BackgroundCreator';
-export { Perimeter } from './components/Perimeter';
-export { BoxCreator } from './creators/BoxCreator';
-export { ButtonCreator } from './creators/ButtonCreator';
-//# sourceMappingURL=index.js.map
