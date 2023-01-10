a5afb01e003b98054922e6b02ec39f686380593b
RGgt <rggt.inbox@gmail.com>
2023-01-10 18:38:30 +0200

    a very messy save-game-screenshot

--- packages/gui-custom-elements/src/creators/dialogs/SaveLoad.ts
+++ packages/gui-custom-elements/src/creators/dialogs/SaveLoad.ts
@@ -1,23 +1,56 @@
-import { BoxCreator, Button, ButtonCreator, GroupBox, PanelBox, Perimeter, TextCreator } from '@rggt/gui-elements';
+import {
+  BoxCreator,
+  Button,
+  ButtonCreator,
+  GroupBox,
+  Highlightable,
+  PanelBox,
+  Perimeter,
+  TextCreator,
+} from '@rggt/gui-elements';
 import AspectConstants from '../../AspectConstants';
 
 export type SaveOptions = {
-  onSave: (page: number, slot: number) => void;
+  onSave: (
+    scene: Phaser.Scene,
+    options: SaveOptions,
+    page: number,
+    slot: number,
+    callbak: (
+      scene: Phaser.Scene,
+      options: SaveOptions,
+      pageIndex: number,
+      slotIndex: number,
+      textureName: string,
+    ) => void,
+  ) => void;
   onClose: () => void;
   getViewData: (page: number, slot: number) => SaveViewData | undefined;
 };
 export type SaveViewData = {
   AutoText: string;
   ImageB64: string;
+  HasData: boolean;
 };
 
-type SaveSlotControls = {
-  perimeter: Perimeter;
-  box: GroupBox;
-  image: Phaser.GameObjects.Sprite;
-  label: Phaser.GameObjects.Text;
-};
+type SaveSlotControls =
+  | {
+    boxHighlightable: Highlightable;
+    box: GroupBox;
+    image?: Phaser.GameObjects.Sprite;
+    label: Phaser.GameObjects.Text;
+  }
+  | undefined;
 class Save {
+  private static _saveSlotControls: Array<SaveSlotControls> = [
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+  ];
+
   public static createSaveDialog(scene: Phaser.Scene, options: SaveOptions) {
     const titleTextValue = 'SAVE-âÂ – ăÂ – țȚ – îÎ – șȘ';
 
@@ -60,23 +93,13 @@ class Save {
     // sprite.setScale((buttonWidth - 50) / 1920, (buttonHeight - 125) / 1080);
     // const box = BoxCreator.createGroupBox(scene, buttonLeft, buttonTop, buttonWidth, buttonHeight);
 
+    const pageIndex = 0;
     const areaLeft = shape.getLeft() + AspectConstants.SAVE_DIALOG_PADDING_H;
     const areaTop =
       shape.getTop() + AspectConstants.SAVE_DIALOG_PADDING_V + titleText.height + AspectConstants.SAVE_DIALOG_SPACING_V;
-    Save._createSlots(scene, options, shape, 0, areaLeft, areaTop, testText.height);
+    Save._createSlots(scene, options, shape, pageIndex, areaLeft, areaTop, testText.height);
 
-    const buttonText = 'Close';
-    const buttonDisabled = false;
-    const btnDetail = ButtonCreator.addSimpleButton(
-      scene,
-      shape.getLeft() + AspectConstants.SAVE_DIALOG_PADDING_H,
-      shape.getBottom() - AspectConstants.SAVE_DIALOG_SPACING_V - AspectConstants.SAVE_DIALOG_BUTTON_HEIGHT,
-      dialogWidth - 2 * AspectConstants.SAVE_DIALOG_PADDING_H,
-      AspectConstants.SAVE_DIALOG_BUTTON_HEIGHT,
-      'Close',
-      options.onClose,
-      false,
-    );
+    Save._createCloseButton(scene, options, shape, dialogWidth);
 
     // let buttonTop = shape.getTop() + AspectConstants.DIALOG_PADDING_V;
     // let buttonLeft = shape.getLeft() + AspectConstants.DIALOG_PADDING_H;
@@ -104,85 +127,226 @@ class Save {
   }
   // private static _placeText()
 
+  private static _createCloseButton(scene: Phaser.Scene, options: SaveOptions, shape: PanelBox, dialogWidth: number) {
+    return ButtonCreator.addSimpleButton(
+      scene,
+      shape.getLeft() + AspectConstants.SAVE_DIALOG_PADDING_H,
+      shape.getBottom() - AspectConstants.SAVE_DIALOG_SPACING_V - AspectConstants.SAVE_DIALOG_BUTTON_HEIGHT,
+      dialogWidth - 2 * AspectConstants.SAVE_DIALOG_PADDING_H,
+      AspectConstants.SAVE_DIALOG_BUTTON_HEIGHT,
+      'Close',
+      options.onClose,
+      false,
+    );
+  }
+
   private static _createSlots(
     scene: Phaser.Scene,
     options: SaveOptions,
     shape: PanelBox,
-    pageNo: number,
+    pageIndex: number,
     areaLeft: number,
     areaTop: number,
     slotTextHeight: number,
   ) {
-    Save._createSaveSlot(scene, options, 1, 0, areaLeft, areaTop, slotTextHeight);
-    Save._createSaveSlot(scene, options, 1, 1, areaLeft, areaTop, slotTextHeight);
-    Save._createSaveSlot(scene, options, 1, 2, areaLeft, areaTop, slotTextHeight);
-    Save._createSaveSlot(scene, options, 1, 3, areaLeft, areaTop, slotTextHeight);
-    Save._createSaveSlot(scene, options, 1, 4, areaLeft, areaTop, slotTextHeight);
-    Save._createSaveSlot(scene, options, 1, 5, areaLeft, areaTop, slotTextHeight);
+    Save._saveSlotControls[0] = Save._createSaveSlot(scene, options, pageIndex, 0, areaLeft, areaTop, slotTextHeight);
+    Save._saveSlotControls[1] = Save._createSaveSlot(scene, options, pageIndex, 1, areaLeft, areaTop, slotTextHeight);
+    Save._saveSlotControls[2] = Save._createSaveSlot(scene, options, pageIndex, 2, areaLeft, areaTop, slotTextHeight);
+    Save._saveSlotControls[3] = Save._createSaveSlot(scene, options, pageIndex, 3, areaLeft, areaTop, slotTextHeight);
+    Save._saveSlotControls[4] = Save._createSaveSlot(scene, options, pageIndex, 4, areaLeft, areaTop, slotTextHeight);
+    Save._saveSlotControls[5] = Save._createSaveSlot(scene, options, pageIndex, 5, areaLeft, areaTop, slotTextHeight);
   }
 
   private static _createSaveSlot(
     scene: Phaser.Scene,
     options: SaveOptions,
-    pageNo: number,
-    slotNo: number,
+    pageIndex: number,
+    slotIndex: number,
     areaLeft: number,
     areaTop: number,
     textHeight: number,
   ) {
     // if there is a game already saved in this slot, load the preview image and text.
-    // const viewData = options.getViewData(pageNo, slotNo);
-    const viewData = true;
+    const viewData = options.getViewData(pageIndex, slotIndex);
+    // const viewData = true;
     const slotWidth = Save._getSlotWidth();
     const slotHeight = Save._getSlotHeight(textHeight);
 
-    let index = slotNo % 3;
+    let index = slotIndex % 3;
     const slotLeft = areaLeft + index * (slotWidth + AspectConstants.SAVE_DIALOG_SPACING_H);
-    index = slotNo - (slotNo % 3);
+    index = slotIndex - (slotIndex % 3);
     const slotTop = areaTop + (index * (slotHeight + AspectConstants.SAVE_DIALOG_SPACING_V)) / 3;
-    if (viewData) {
-      // sprite
-      const sprite = scene.add.sprite(
-        slotLeft + AspectConstants.SAVE_DIALOG_SLOT_PADDING_H,
-        slotTop + AspectConstants.SAVE_DIALOG_SLOT_PADDING_V,
-        'frame_1_08_10',
-      );
-      sprite.setOrigin(0, 0);
-      sprite.setScale(
-        AspectConstants.SAVE_DIALOG_SLOT_IMAGE_WIDTH / 1920,
-        AspectConstants.SAVE_DIALOG_SLOT_IMAGE_HEIGHT / 1080,
-      );
-
-      // text
-      const textLeft =
-        slotLeft + AspectConstants.SAVE_DIALOG_SLOT_PADDING_H + AspectConstants.SAVE_DIALOG_SLOT_IMAGE_WIDTH / 2;
-      const textTop =
-        slotTop +
-        AspectConstants.SAVE_DIALOG_SLOT_IMAGE_HEIGHT +
-        AspectConstants.SAVE_DIALOG_SLOT_SPACING_V +
-        AspectConstants.SAVE_DIALOG_SLOT_PADDING_V;
-      const boxText = TextCreator.createSaveButtonText(
+    if (viewData && viewData.HasData) {
+      return Save._createPopulatedSlot(
+        viewData,
         scene,
-        textLeft,
-        textTop,
-        'Friday, October 15 2021, \r\n23:42',
-        AspectConstants.SAVE_DIALOG_SLOT_IMAGE_WIDTH,
+        options,
+        pageIndex,
+        slotIndex,
+        slotLeft,
+        slotTop,
+        slotWidth,
+        slotHeight,
       );
-      boxText.setOrigin(0.5, 0);
-      scene.add.existing(boxText);
-
-      // box
-      const box = BoxCreator.createGroupBox(scene, slotLeft, slotTop, slotWidth, slotHeight);
-
-      // perimeter
-      // const boxPerimeter = BoxCreator.createPerimeter(scene, slotLeft, slotTop, slotWidth, slotHeight);
-      // boxPerimeter.reactToClick = options.onSave;
-
-      // Highlightable
-      const boxHighlightable = BoxCreator.createHighlightable(scene, slotLeft, slotTop, slotWidth, slotHeight);
-      boxHighlightable.onClick = options.onClose;
-      // create an image
     }
+    return Save._createEmptySlot(scene, options, pageIndex, slotIndex, slotLeft, slotTop, slotWidth, slotHeight);
+  }
+
+  private static _createPopulatedSlot(
+    viewData: SaveViewData,
+    scene: Phaser.Scene,
+    options: SaveOptions,
+    pageIndex: number,
+    slotIndex: number,
+    slotLeft: number,
+    slotTop: number,
+    slotWidth: number,
+    slotHeight: number,
+  ) {
+    const sprite = scene.add.sprite(
+      slotLeft + AspectConstants.SAVE_DIALOG_SLOT_PADDING_H,
+      slotTop + AspectConstants.SAVE_DIALOG_SLOT_PADDING_V,
+      // 'frame_1_08_10',
+      `screenshot_${pageIndex}_${slotIndex}`,
+    );
+    sprite.setOrigin(0, 0);
+    sprite.setScale(
+      (4 * AspectConstants.SAVE_DIALOG_SLOT_IMAGE_WIDTH) / 1920,
+      (4 * AspectConstants.SAVE_DIALOG_SLOT_IMAGE_HEIGHT) / 1080,
+    );
+
+    // text
+    const textLeft =
+      slotLeft + AspectConstants.SAVE_DIALOG_SLOT_PADDING_H + AspectConstants.SAVE_DIALOG_SLOT_IMAGE_WIDTH / 2;
+    const textTop =
+      slotTop +
+      AspectConstants.SAVE_DIALOG_SLOT_IMAGE_HEIGHT +
+      AspectConstants.SAVE_DIALOG_SLOT_SPACING_V +
+      AspectConstants.SAVE_DIALOG_SLOT_PADDING_V;
+    const boxText = TextCreator.createSaveButtonText(
+      scene,
+      textLeft,
+      textTop,
+      // 'Friday, October 15 2021, \r\n23:42',
+      viewData.AutoText,
+      AspectConstants.SAVE_DIALOG_SLOT_IMAGE_WIDTH,
+    );
+    boxText.setOrigin(0.5, 0);
+    scene.add.existing(boxText);
+
+    // box
+    const box = BoxCreator.createGroupBox(scene, slotLeft, slotTop, slotWidth, slotHeight);
+
+    // perimeter
+    // const boxPerimeter = BoxCreator.createPerimeter(scene, slotLeft, slotTop, slotWidth, slotHeight);
+    // boxPerimeter.reactToClick = options.onSave;
+
+    // Highlightable
+    const boxHighlightable = BoxCreator.createHighlightable(scene, slotLeft, slotTop, slotWidth, slotHeight);
+    boxHighlightable.onClick = () => {
+      Save._callOnSaveMethod(scene, options, pageIndex, slotIndex);
+    };
+    const slotControls: SaveSlotControls = {
+      boxHighlightable,
+      box,
+      image: sprite,
+      label: boxText,
+    };
+    return slotControls;
+  }
+
+  private static _createEmptySlot(
+    scene: Phaser.Scene,
+    options: SaveOptions,
+    pageIndex: number,
+    slotIndex: number,
+    slotLeft: number,
+    slotTop: number,
+    slotWidth: number,
+    slotHeight: number,
+  ) {
+    // box
+    const box = BoxCreator.createGroupBox(scene, slotLeft, slotTop, slotWidth, slotHeight);
+
+    // text
+    const boxText = TextCreator.createTitleText(
+      scene,
+      box.getCenter().x,
+      box.getCenter().y,
+      'Free Slot',
+      AspectConstants.SAVE_DIALOG_SLOT_IMAGE_WIDTH,
+    );
+    boxText.setOrigin(0.5, 0.5);
+    scene.add.existing(boxText);
+
+    // highlightable
+    const boxHighlightable = BoxCreator.createHighlightable(scene, slotLeft, slotTop, slotWidth, slotHeight);
+    boxHighlightable.onClick = () => {
+      Save._callOnSaveMethod(scene, options, pageIndex, slotIndex);
+    };
+    const slotControls: SaveSlotControls = {
+      boxHighlightable,
+      box,
+      image: undefined,
+      label: boxText,
+    };
+    return slotControls;
+  }
+
+  private static onSaveCompleted(
+    scene: Phaser.Scene,
+    options: SaveOptions,
+    pageIndex: number,
+    slotIndex: number,
+    textureName: string,
+  ) {
+    scene.game.scene.wake(scene);
+
+    // re-create slot
+    // TODO: fix messy
+    const testText = TextCreator.createSaveButtonText(
+      scene,
+      0,
+      0,
+      'fq\r\nfq',
+      AspectConstants.SAVE_DIALOG_SLOT_IMAGE_WIDTH,
+    );
+    const titleTextValue = 'SAVE-âÂ – ăÂ – țȚ – îÎ – șȘ';
+    const dialogWidth = Save._getDialogWidth();
+    const titleText = TextCreator.createTitleText(scene, 0, 0, titleTextValue, dialogWidth);
+    const dialogHeight = Save._getDialogHeight(titleText.height, testText.height);
+
+    const shapeLeft = (1920 - dialogWidth) / 2;
+    const shapeTop = (1080 - dialogHeight) / 2;
+
+    const areaLeft = shapeLeft /* shape.getLeft() */ + AspectConstants.SAVE_DIALOG_PADDING_H;
+    const areaTop =
+      shapeTop /* shape.getTop() */ +
+      AspectConstants.SAVE_DIALOG_PADDING_V +
+      titleText.height +
+      AspectConstants.SAVE_DIALOG_SPACING_V;
+    const slotTextHeight = testText.height;
+    Save._saveSlotControls[slotIndex] = Save._createSaveSlot(
+      scene,
+      options,
+      pageIndex,
+      slotIndex,
+      areaLeft,
+      areaTop,
+      slotTextHeight,
+    );
+  }
+
+  private static _callOnSaveMethod(scene: Phaser.Scene, options: SaveOptions, pageIndex: number, slotIndex: number) {
+    const controls = Save._saveSlotControls[slotIndex];
+    if (controls) {
+      controls.box.destroy(true);
+      controls.boxHighlightable.destroy();
+      controls.label.destroy();
+      controls.image?.destroy();
+    }
+    scene.game.scene.sleep(scene);
+    options.onSave(scene, options, pageIndex, slotIndex, Save.onSaveCompleted);
   }
 
   private static _getSlotWidth() {
--- src/scenes/SaveScene.ts
+++ src/scenes/SaveScene.ts
@@ -8,6 +8,15 @@ export default class SaveScene extends Phaser.Scene {
     this.events = new Phaser.Events.EventEmitter();
   }
 
+  private _savedData: SaveViewData[] = [
+    { HasData: false, AutoText: 'dddd', ImageB64: 'ddd' },
+    { HasData: false, AutoText: 'dddd', ImageB64: 'ddd' },
+    { HasData: false, AutoText: 'dddd', ImageB64: 'ddd' },
+    { HasData: false, AutoText: 'dddd', ImageB64: 'ddd' },
+    { HasData: false, AutoText: 'dddd', ImageB64: 'ddd' },
+    { HasData: false, AutoText: 'dddd', ImageB64: 'ddd' },
+  ];
+
   create() {
     const options: SaveOptions = {
       onSave: this.onSave.bind(this),
@@ -24,11 +33,152 @@ export default class SaveScene extends Phaser.Scene {
     this.game.scene.sleep(SPScenes.Save);
   }
 
-  onSave(page: number, slot: number) {
+  onSave(
+    scene: Phaser.Scene,
+    options: SaveOptions,
+    pageIndex: number,
+    slotIndex: number,
+    callbak: (
+      scene: Phaser.Scene,
+      options: SaveOptions,
+      pageIndex: number,
+      slotIndex: number,
+      textureName: string,
+    ) => void,
+  ) {
+    this._savedData[slotIndex].HasData = true;
+    this._tempSave(scene, options, pageIndex, slotIndex, callbak);
     return;
   }
 
   getViewData(page: number, slot: number): SaveViewData {
-    return { AutoText: 'Friday, October 15 2021, 23:42', ImageB64: 'base-64' };
+    return this._savedData[slot];
+    // return { AutoText: 'Friday, October 15 2021, 23:42', ImageB64: 'base-64' };
   }
+
+  private static _screenshotScaleFactor = 0.25;
+
+  private _screenshotSprite: Phaser.GameObjects.Sprite | undefined;
+
+  private _tempSave(
+    scene: Phaser.Scene,
+    options: SaveOptions,
+    pageIndex: number,
+    slotIndex: number,
+    callbak: (
+      scene: Phaser.Scene,
+      options: SaveOptions,
+      pageIndex: number,
+      slotIndex: number,
+      textureName: string,
+    ) => void,
+  ) {
+    this.renderer.snapshot((snapshot) => {
+      const targetWidth = 1920 * SaveScene._screenshotScaleFactor;
+      const targetHeight = 1080 * SaveScene._screenshotScaleFactor;
+      const dataURL = SaveScene.screenshotToBase64(snapshot as HTMLImageElement, targetWidth, targetHeight);
+      this._savedData[slotIndex].AutoText = new Date().toLocaleString();
+      this.base64ToSprite(dataURL, scene, options, pageIndex, slotIndex, callbak);
+    });
+  }
+
+  private static screenshotToBase64 = (screenshot: HTMLImageElement, targetWidth: number, targetHeight: number) => {
+    const scaledDownCanvas = document.createElement('canvas') as HTMLCanvasElement;
+    scaledDownCanvas.width = targetWidth;
+    scaledDownCanvas.height = targetHeight;
+    const context = scaledDownCanvas.getContext('2d') as CanvasRenderingContext2D;
+    context.drawImage(
+      screenshot,
+      0,
+      0,
+      screenshot.width,
+      screenshot.height,
+      0,
+      0,
+      scaledDownCanvas.width,
+      scaledDownCanvas.height,
+    );
+
+    return scaledDownCanvas.toDataURL();
+  };
+
+  private base64ToSprite = (
+    base64DataUrl: string,
+    scene: Phaser.Scene,
+    options: SaveOptions,
+    pageIndex: number,
+    slotIndex: number,
+    callbak: (
+      scene: Phaser.Scene,
+      options: SaveOptions,
+      pageIndex: number,
+      slotIndex: number,
+      textureName: string,
+    ) => void,) => {
+    // /////////////////////////////////////////////////
+    // Alternative using addBase64:
+    //
+    // this.textures.once('addtexture', function () {
+    //   this.add.image(400, 300, textureName);
+    // }, this);
+    // this.textures.addBase64(textureName, dataURL);
+    // /////////////////////////////////////////////////
+    const textureName = `screenshot_${pageIndex}_${slotIndex}`;
+    const image = new Image();
+    image.crossOrigin = 'anonymous';
+    image.addEventListener('error', (e: ErrorEvent) => {
+      console.log(`Error: ${e} || ${e.target}`, e, this);
+    });
+
+    image.addEventListener('load', () => {
+      if (this._screenshotSprite) {
+        this._screenshotSprite.destroy();
+        this.textures.remove(textureName);
+      }
+      this.textures.remove(textureName);
+      this.textures.addImage(textureName, image);
+
+      const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
+      const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
+      this._screenshotSprite = this.add.sprite(screenCenterX, screenCenterY, textureName);
+      this._screenshotSprite.setOrigin(0.0);
+      this._screenshotSprite.setPosition(0, 1080 * (1 - SaveScene._screenshotScaleFactor));
+      callbak?.(scene, options, pageIndex, slotIndex, textureName);
+    });
+    image.src = base64DataUrl;
+  };
+
+  private base64ToSprite_OLD = (base64DataUrl: string, pageIndex: number, slotIndex: number) => {
+    // /////////////////////////////////////////////////
+    // Alternative using addBase64:
+    //
+    // this.textures.once('addtexture', function () {
+    //   this.add.image(400, 300, textureName);
+    // }, this);
+    // this.textures.addBase64(textureName, dataURL);
+    // /////////////////////////////////////////////////
+    const textureName = `screenshot_${pageIndex}_${slotIndex}`;
+    const image = new Image();
+    image.crossOrigin = 'anonymous';
+    image.addEventListener('error', (e: ErrorEvent) => {
+      console.log(`Error: ${e} || ${e.target}`, e, this);
+    });
+
+    image.addEventListener('load', () => {
+      if (this._screenshotSprite) {
+        this._screenshotSprite.destroy();
+        this.textures.remove(textureName);
+      }
+      this.textures.remove(textureName);
+      this.textures.addImage(textureName, image);
+      console.log(`writting screenshot_${pageIndex}_${slotIndex}`);
+
+      const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
+      const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
+      this._screenshotSprite = this.add.sprite(screenCenterX, screenCenterY, textureName);
+      this._screenshotSprite.setOrigin(0.0);
+      this._screenshotSprite.setPosition(0, 1080 * (1 - SaveScene._screenshotScaleFactor));
+    });
+    image.src = base64DataUrl;
+  };
 }
