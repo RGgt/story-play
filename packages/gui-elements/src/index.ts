11d430abcd734ff789524104f49863d0ca5ceafa
RGgt <rggt.inbox@gmail.com>
2023-01-12 15:45:04 +0200

    fixed package exports

--- packages/gui-elements/src/index.ts
+++ packages/gui-elements/src/index.ts
@@ -1,9 +1,10 @@
+export { BackgroundBlocker } from './components/BackgroundBlocker';
 export { default as Button } from './components/Button';
-export { default as PanelBox } from './components/PanelBox';
 export { default as GroupBox } from './components/GroupBox';
-export * from './creators/TextCreator';
-export { BackgroundCreator } from './creators/BackgroundCreator';
-export { Perimeter } from './components/Perimeter';
 export { Highlightable } from './components/Highlightable';
+export { default as PanelBox } from './components/PanelBox';
+export { Perimeter } from './components/Perimeter';
+export { BackgroundCreator } from './creators/BackgroundCreator';
 export { BoxCreator } from './creators/BoxCreator';
 export { ButtonCreator } from './creators/ButtonCreator';
+export * from './creators/TextCreator';
